1b80343b7cf28f8f165adcbd987dac87deb5256d
Bin Huang <huangbinapple@gmail.com>
2023-03-29 23:22:21 +0800

    Add file
    add protein class

---
+++ le2/common/protein.py
@@ -0,0 +1,174 @@
+"""Protein data type."""
+import os
+import io
+from Bio.PDB import PDBParser, FastMMCIFParser
+import torch
+
+
+def sum_two_numbers(a, b):
+    return a + b
+
+
+def extract_main_chain_atoms(structure):
+  """
+  Extracts the main chain atoms (N, CA, C) for each residue in a Bio.PDB structure object and
+  returns a dictionary containing the main chain atoms as a torch.tensor array of size (L, 3, 3),
+  where L is the number of residues in the structure. The second dimension represents the three
+  main chain atoms (N, CA, C) and the third dimension represents their (x, y, z) coordinates.
+  Also returns three lists of length L, storing the residue name, residue index, and chain ID,
+  respectively.
+
+  If any of the main chain atoms are missing for a residue, their coordinates will be set to NaN.
+
+  Args:
+  - structure (Bio.PDB.Structure): a Bio.PDB structure object containing a protein structure
+
+  Returns:
+  - output (dict): a dictionary containing the following fields:
+    - main_chain_atoms (torch.tensor): a torch.tensor array of size (L, 3, 3) containing the
+        coordinates of the main chain atoms (N, CA, C) for each residue in the structure
+    - residue_names (list): a list of length L containing the residue names for each residue in
+        the structure
+    - residue_indices (list): a list of length L containing the residue indices for each residue in
+        the structure
+    - chain_ids (list): a list of length L containing the chain IDs for each residue in the structure
+    - multiple_models (bool): a boolean flag indicating whether the structure contains multiple
+        models (True) or not (False)
+  """
+  # Check if the structure contains multiple models
+  num_models = len(structure)
+  multiple_models = num_models > 1
+  
+  # Select the first model if there are multiple models
+  if multiple_models:
+    model = structure[0]
+  else:
+    model = structure
+
+  # Initialize the lists for the output fields
+  main_chain_atoms = []
+  residue_names = []
+  residue_indices = []
+  chain_ids = []
+
+  # Loop through each residue in the structure
+  for residue in model.get_residues():
+    if residue.id[0] != ' ':  # Ignore hetero residues
+      continue
+    if residue.id[2] != ' ':  # Encountered an insertion code
+      raise ValueError('Encountered an insertion code: {} in chain {}'.format(
+        residue.id, residue.get_parent().id))
+    
+    residue_atoms = []
+
+    # Get the coordinates of the CA, C, and N atoms for the residue
+    for atom_name in ['N', 'CA', 'C']:
+      try:
+        atom = residue[atom_name]
+        coords = torch.tensor(atom.get_coord())
+      except KeyError:
+        coords = torch.tensor([float('nan')] * 3)
+      residue_atoms.append(coords)
+
+    main_chain_atoms.append(residue_atoms)
+    residue_names.append(residue.get_resname())
+    residue_indices.append(residue.get_id()[1])
+    chain_ids.append(residue.get_parent().id)
+
+  main_chain_atoms = torch.stack(
+    [torch.stack(residue_atoms) for residue_atoms in main_chain_atoms])
+
+  # Create the output dictionary
+  output = {
+      'main_chain_atoms': main_chain_atoms,  # shape: (L, 3, 3)
+      'residue_names': residue_names,  # elements: str
+      'residue_indices': residue_indices,  # elements: int
+      'chain_ids': chain_ids,  # elements: str
+      'multiple_models': multiple_models,  # bool
+  }
+
+  return output
+
+
+class Protein:
+  """Protein structure representation."""
+
+  def __init__(self, raw_string, file_type='pdb'):
+    """Initialize a Protein object.
+
+    Args:
+    - raw_string (str): a string containing the path to the input file
+    - file_type (str): the type of the input file, either 'pdb' or 'cif'
+    """
+    self._raw_string = raw_string
+    self._file_type = file_type
+    self._structure = self._load_structure()
+    self._atoms_info = extract_main_chain_atoms(self._structure)
+    self._valid_residues_index =\
+      (~self._atoms_info['main_chain_atoms'].sum(dim=(1, 2)).isnan()).\
+        nonzero().squeeze()
+    self._atom_coords =\
+      self._atoms_info['main_chain_atoms'][self._valid_residues_index]
+      # shape: (L, 3, 3)
+    self.residue_names =\
+      [self._atoms_info['residue_names'][i] for i in self._valid_residues_index]
+    residue_indicies =\
+      [self._atoms_info['residue_indices'][i] for i in self._valid_residues_index]
+    residue_chain_ids =\
+      [self._atoms_info['chain_ids'][i] for i in self._valid_residues_index]
+    # Create a sequence coordinates for each residue, combining its chain ID
+    # and index
+    residu_seq_coordinates =\
+      torch.tensor(residue_indicies) +\
+        torch.tensor([hash(chain_id) for chain_id in residue_chain_ids])
+    # Calculate the distances between all residues in seqeunce space
+    self.mutual_seqeunce_distance =\
+      residu_seq_coordinates.unsqueeze(1) - residu_seq_coordinates
+      # Shape: (L, L)
+      
+    # Calculate the distances between all pairs of CA atoms in 3D space
+    self.mutual_ca_distances = self._calculate_mutual_ca_distances()
+    # Shape: (L, L)
+    
+  def __len__(self):
+    """Return the number of residues in the protein."""
+    return len(self._atom_coords)
+    
+  def _load_structure(self):
+    """Load the protein structure from the input file."""
+    if self._file_type == 'pdb':
+      parser = PDBParser()
+    elif self._file_type == 'cif':
+      parser = FastMMCIFParser()
+    else:
+      raise ValueError('Invalid file type: {}'.format(self._file_type))
+    handle = io.StringIO(self._raw_string)
+    return parser.get_structure('protein', handle)
+  
+  def _calculate_mutual_ca_distances(self):
+    """Calculate the distances between all pairs of CA atoms in the protein."""
+    # Calculate the distances between all pairs of CA atoms
+    ca_atoms = self._atom_coords[:, 1, :]  # size: (L, 3)
+    distances = torch.cdist(ca_atoms, ca_atoms)
+    return distances  # shape: (L, L)
+  
+  def get_neighbor_indicies(self, residue_index, cutoff=12.0):
+    """Get the neighbors of a residue.
+
+    Args:
+    - residue_index (int): the index of the residue
+    - cutoff (float): the cutoff distance for defining a neighbor
+
+    Returns:
+    - neighbors (torch.tensor): a troch tensor of type int containing the
+    indices of the neighboring redidues
+    """
+    # Add cutoff to the diagonal to avoid self-neighborship
+    ca_distances = self.mutual_ca_distances +\
+      torch.eye(self.mutual_ca_distances.shape[0]) * cutoff
+    return (ca_distances[residue_index] < cutoff).nonzero().squeeze()
+
+
+if __name__ == '__main__':
+  import doctest
+  doctest.testmod()
---
+++ le2/common/test_protein.py
@@ -0,0 +1,151 @@
+import torch
+import unittest
+from Bio.PDB import PDBParser, FastMMCIFParser
+import numpy as np
+from le2.common.protein import extract_main_chain_atoms, Protein
+
+
+class TestExtractMainChainAtoms(unittest.TestCase):
+
+  def setUp(self):
+    # Load the example PDB file
+    self.structure_from_pdb = PDBParser(QUIET=True).get_structure(
+      'example', 'example/0089.pdb')
+    self.structure_from_cif = FastMMCIFParser(QUIET=True).get_structure(
+      'example', 'example/1K12.cif')
+    self.structure_from_cif_multi_model = FastMMCIFParser(QUIET=True).get_structure(
+      'example', 'example/1E17.cif')
+    self.structure_from_cif_insertion_code = FastMMCIFParser(QUIET=True).get_structure(
+      'example', 'example/1KB5.cif')
+
+  def test_pdb_as_input(self):
+    # Extract output from output dict of extract_main_chain_atoms
+    main_chain_atom_infos = extract_main_chain_atoms(self.structure_from_pdb)
+    main_chain_atoms = main_chain_atom_infos['main_chain_atoms']
+    residue_names = main_chain_atom_infos['residue_names']
+    residue_indices = main_chain_atom_infos['residue_indices']
+    chain_ids = main_chain_atom_infos['chain_ids']
+    is_multiple_models = main_chain_atom_infos['multiple_models']
+
+    # Check that the main chain atoms tensor has the correct shape
+    self.assertEqual(main_chain_atoms.shape, (100, 3, 3))
+
+    # Check that the first residue is Glycine (GLY)
+    self.assertEqual(residue_names[0], 'SER')
+
+    # Check that the first residue index is 1
+    self.assertEqual(residue_indices[0], 1)
+
+    # Check that the first residue is in chain A
+    self.assertEqual(chain_ids[0], 'A')
+
+    # Check that the coordinates of the first residue's CA atom are correct
+    self.assertTrue(torch.allclose(main_chain_atoms[0][1],
+                                   torch.tensor([2.687, -0.122, 0.339])))
+
+    # Check that the coordinates of the first residue's C atom are correct
+    # (missing atom in the PDB file)
+    assert torch.isnan(main_chain_atoms[0][2]).all()
+
+    # Check that the coordinates of the first residue's N atom are correct
+    self.assertTrue(torch.allclose(main_chain_atoms[0][0],
+                                   torch.tensor([1.240, -0.218, 0.229])))
+    
+    # Check that the structure has only one model
+    self.assertTrue(is_multiple_models == False)
+    
+  
+  def test_cif_as_input(self):
+
+    # Extract output from output dict of extract_main_chain_atoms.
+    main_chain_atom_infos = extract_main_chain_atoms(self.structure_from_cif)
+    main_chain_atoms = main_chain_atom_infos['main_chain_atoms']
+    residue_names = main_chain_atom_infos['residue_names']
+    residue_indices = main_chain_atom_infos['residue_indices']
+    chain_ids = main_chain_atom_infos['chain_ids']
+    is_multiple_models = main_chain_atom_infos['multiple_models']
+
+    # Check that the main chain atoms tensor has the correct shape
+    self.assertEqual(main_chain_atoms.shape, (158, 3, 3))
+
+    # Check that the first residue is Glycine (GLY)
+    self.assertEqual(residue_names[0], 'VAL')
+
+    # Check that the first residue index is 1
+    self.assertEqual(residue_indices[0], 1)
+
+    # Check that the first residue is in chain A
+    self.assertEqual(chain_ids[0], 'A')
+
+    # Check that the coordinates of the first residue's CA atom are correct
+    self.assertTrue(torch.allclose(main_chain_atoms[0][1], torch.tensor(
+      [-18.860, 19.552, 33.174])))
+
+    # Check that the coordinates of the first residue's C atom are correct
+    # (missing atom in the PDB file)
+    assert torch.isnan(main_chain_atoms[0][2]).all()
+
+    # Check that the coordinates of the first residue's N atom are correct
+    self.assertTrue(torch.allclose(main_chain_atoms[0][0], torch.tensor(
+      [-19.818, 18.723, 32.377])))
+    
+    # Check that the structure has only one model
+    self.assertTrue(is_multiple_models == False)
+    
+  def test_multi_model_cif(self):
+    
+    # Extract output from output dict of extract_main_chain_atoms
+    main_chain_atom_infos = extract_main_chain_atoms(
+      self.structure_from_cif_multi_model)
+    main_chain_atoms = main_chain_atom_infos['main_chain_atoms']
+    is_multiple_models = main_chain_atom_infos['multiple_models']
+
+    # Check that the main chain atoms tensor has the correct shape
+    self.assertEqual(main_chain_atoms.shape, (90, 3, 3))
+    
+    # Check that the structure has multiple models.
+    self.assertTrue(is_multiple_models == True)
+    
+  def test_cif_with_insertion_code(self):
+    with self.assertRaises(ValueError):
+      extract_main_chain_atoms(self.structure_from_cif_insertion_code)
+
+
+class TestProtein(unittest.TestCase):
+  
+  def setUp(self):
+    pdb_file = "example/0089.pdb"
+    with open(pdb_file, 'r') as f:
+      raw_string = f.read()
+    self.protein = Protein(raw_string, file_type="pdb")
+
+  def test_load_structure(self):
+    # Check that the structure is loaded correctly
+    self.assertIsNotNone(self.protein._structure)
+
+  def test_get_neighbors(self):
+    # Check that the neighbors are correctly computed
+    test_i = 10
+    neighbors_5 = self.protein.get_neighbor_indicies(test_i, cutoff=5)
+    self.assertIn(test_i - 1, neighbors_5)
+    self.assertIn(test_i + 1, neighbors_5)
+    self.assertNotIn(test_i, neighbors_5)
+    neighbors_10 = self.protein.get_neighbor_indicies(test_i, cutoff=10)
+    # print('number of neighbors 5: ', neighbors_5.size())
+    # print('number of neighbors 10: ', neighbors_10.size())
+    self.assertTrue(neighbors_5.size() < neighbors_10.size())
+
+  def test_sequence_distance(self):
+    # Check that the sequence distance is correctly computed
+    test_i = 10
+    distance = self.protein.mutual_seqeunce_distance[test_i + 1, test_i].item()
+    self.assertEqual(distance, 1)
+
+  def test_ca_distance(self):
+    # Check that the CA distance is correctly computed
+    CA0_coords = [2.687, -0.122, 0.339]
+    CA1_coords = [4.911, 2.776, 1.347]
+    true_distance =\
+      np.linalg.norm(np.array(CA0_coords) - np.array(CA1_coords))
+    distance = self.protein.mutual_ca_distances[0, 1].item()
+    self.assertAlmostEqual(distance, true_distance, places=1)
