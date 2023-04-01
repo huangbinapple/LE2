d834e51c3ec1564f9a6456fc11d71b30b92fc042
Bin Huang <huangbinapple@gmail.com>
2023-04-01 13:48:44 +0800

    Add: function for making input feature to N.N.

---
+++ le2/model/feature.py
@@ -0,0 +1,54 @@
+import torch
+from le2.common import r3
+from le2.common import residue_constants as rc
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_sequence
+
+
+def make_feature(feature_batch: dict, device: str ='cpu',
+                 max_1d_distance: int = 5) -> dict:
+  """
+  Make features that fed into the model.
+  """
+  # relative position in sequence (one hot encode)
+  target_position_1d = \
+    feature_batch['target_index'] + feature_batch['target_chain_id']
+  neighbor_position_1d = \
+    feature_batch['neighbor_indicies'] + feature_batch['neighbor_chain_ids']
+  r_position_1d = neighbor_position_1d - target_position_1d.unsqueeze(1)
+  r_position_1d = torch.clamp(r_position_1d + max_1d_distance,
+                              min=0, max=max_1d_distance * 2)
+  r_position_1d = F.one_hot(r_position_1d, num_classes=max_1d_distance * 2 + 1)
+  # Shape: (B, L, 11)  # default max_1d_distance = 5
+  
+  # relative position in 3d space
+  target_frame = r3.vec2transform(feature_batch['target_atom_coordinates'])
+  # Shape: (B, 4, 4)
+  neighbor_frame = r3.vec2transform(feature_batch['neighbor_atom_coordinates'])
+  # Shape: (B, L, 4, 4)
+  r_position_3d = torch.einsum('Bij,BLjk->BLik',
+    r3.transform_invert(target_frame), neighbor_frame)
+  # Shape: (B, L, 4, 4)
+  r_position_3d = r3.transform2feat(r_position_3d)
+  # Shape: (B, L, 12)
+  
+  # residue type (one hot encode)
+  residue_type = F.one_hot(
+    feature_batch['neighbor_names'], num_classes=rc.restype_num + 1)
+  # Shape: (B, L, 21)
+  
+  # is on the same chain
+  is_same_chain = feature_batch['neighbor_chain_ids'] - \
+    feature_batch['target_chain_id'].unsqueeze(1)
+  # Shape: (B, L)
+  is_same_chain = is_same_chain.unsqueeze(-1)
+  # Shape: (B, L, 1)
+  
+  # Concate all features.
+  return torch.cat([
+    r_position_1d,
+    r_position_3d,
+    residue_type,
+    is_same_chain,], dim=-1)
+  # Shape: (B, L, 45) 11 + 12 + 21 + 1
+  
---
+++ le2/model/test_feature.py
@@ -0,0 +1,21 @@
+import unittest
+import torch
+from le2.data.dataset import LocalEnvironmentDataSet, collate_fn
+from torch.utils.data import DataLoader
+from le2.model.feature import make_feature
+
+
+# Test make_feature
+class TestMakeFeature(unittest.TestCase):
+  
+  def setUp(self):
+    self.dataset = LocalEnvironmentDataSet('example/0089.pdb')
+    self.sample = next(iter(DataLoader(
+      self.dataset, batch_size=2, collate_fn=collate_fn)))
+    
+  # Test the output shape of make_feature
+  def test_shape(self):
+    feature = make_feature(self.sample['features'])
+    max_length = self.sample['features']['neighbor_names'].shape[1]
+    self.assertEqual(feature.shape, (2, max_length, 45))
+    self.assertIsInstance(feature, torch.FloatTensor)
