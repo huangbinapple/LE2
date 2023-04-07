52ddd8c0a1bd3638a7c5a8705a8e77ac3247a421
Bin Huang <huangbinapple@gmail.com>
2023-04-08 01:40:00 +0800

    Update: the output interface of forward().
    Only return selected items to improve efficiency.

--- le2/model/modules.py
+++ le2/model/modules.py
@@ -23,7 +23,9 @@ class ResidueTypePredictor(nn.Module):
     # Define the output layer.
     self.head_residue_type = nn.Linear(d_model, rc.restype_num + 1, device=device)
 
-  def forward(self, sample: dict, compute_loss=False) -> dict:
+  def forward(self, sample: dict, output_loss=False,
+              output_logit=False, output_confidence=False,
+              output_predicted_rtype=False, output_ncorrect=False) -> dict:
     """
     Args:
       - sample: dict of input (See le2/data/dataset.py/collate_fn).
@@ -44,14 +46,30 @@ class ResidueTypePredictor(nn.Module):
     x = torch.sum(x, dim=1)  # Shape: (B, 256)
     x = x / torch.sum(mask, dim=1, keepdim=True) # Shape: (B, 256)
     logits = self.head_residue_type(x)  # Shape: (B, 21)
-    output['logit'] = logits
     
-    if compute_loss:
-      self.loss(output, sample)
+    # Calculation only depends on the logits.
+    if output_logit:
+      output['logit'] = logits
+    if output_predicted_rtype or output_ncorrect:
+      predicted_rtypes = torch.argmax(logits, dim=1)  # Shape: (B,)
+      if output_predicted_rtype:
+        output['predicted_rtype'] = predicted_rtypes
+    if output_confidence or output_confidence:
+      probabilties = torch.softmax(logits, dim=1)  # Shape: (B, 21)
+      if output_confidence:
+        output['probability'] = probabilties
+      if output_confidence:
+        # Calculate the confidence, which is the entropy of the probability
+        confidence = (probabilties * torch.log(probabilties)).sum(1) # Shape: (B,)
+        output['confidence'] = confidence
+    
+    # Calculation depends on the target.
+    if output_loss or output_ncorrect:
+      target_names = sample['labels']['target_name'].to(self.device)
+      if output_loss:
+        output['loss'] = nn.CrossEntropyLoss(reduction='none')(
+        logits, target_names.to(self.device))      
+      if output_ncorrect:
+        output['ncorrect'] = (predicted_rtypes == target_names).sum()
+      
     return output
-  
-  def loss(self, output, sample):
-    """Compute loss."""
-    target_names = sample['labels']['target_name'].to(self.device)
-    output['loss'] = nn.CrossEntropyLoss(reduction='none')(
-      output['logit'], target_names.to(self.device))
