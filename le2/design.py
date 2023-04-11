c39f860c4cd607aae6617c11234d462e2a8fb3b9
Bin Huang <huangbinapple@gmail.com>
2023-04-11 20:03:04 +0800

    add: runnable desgin script, but BUGS exists.
    The designed seq has high loss and low acc.

---
+++ le2/design.py
@@ -0,0 +1,194 @@
+import argparse
+import os
+import json
+import time
+import random
+import torch
+from torch.utils.data import DataLoader, Subset, SequentialSampler
+from le2.common import log
+from le2.model.modules import ResidueTypePredictor
+from le2.data.dataset import LocalEnvironmentDataSet, collate_fn, \
+  construct_dataset_from_dir
+from le2.common import residue_constants as rc
+
+logger = log.logger
+
+
+class SequenceDesigner():
+  
+  def __init__(self, model):
+    self.radius = 12
+    self.model = model
+    self.model.eval()
+    self.dataset = None
+    self.is_correct = None
+    self.loss = None
+    self.predicted_rtype = None
+    
+  def _initialize_seq(self):
+    """Randomly initialize the sequence."""
+    # Set each position to a random residue type.
+    for i in range(len(self.seq)):
+      self.seq[i] = rc.resnames[random.randint(0, 19)]
+    
+  def _iter(self, index):
+    """Change residues at index to their predicted rtype."""
+    if self.dataset is None:
+      raise ValueError('No dataset loaded')
+    neighbor_index = set()
+    # Change residues and collect affected neighbors.
+    for i, residue_index in zip(index, self.predicted_rtype):
+      original_residue = self.seq[i]
+      self.seq[i] = rc.resnames[residue_index]
+      logger.debug(
+        f"Residue {i}({original_residue}) changed to {rc.resnames[residue_index]}")
+      neighbor_index.update(
+        self.protein.get_neighbor_indicies(i, self.radius).tolist())
+    neighbor_index = list(neighbor_index)
+    # Update neighbors' states.
+    update_output = self._predict(neighbor_index)
+    self.is_correct[neighbor_index] = update_output['iscorrect']
+    self.loss[neighbor_index] = update_output['loss']
+    self.predicted_rtype[neighbor_index] = update_output['predicted_rtype']
+    logger.debug(f"Updated {len(neighbor_index)} residues")
+    logger.debug(f'current sequence:\t {self.long_to_short_seq(self.seq)}')
+    predict_seq = ''.join(rc.restypes[i] for i in self.predicted_rtype)
+    logger.debug(f'predicted sequence:\t {predict_seq}')
+    logger.debug(f"loss: {self.get_loss()}; accuracy: {self.get_accuracy()}")
+    
+  def _predict(self, index=None):
+    if self.dataset is None:
+      raise ValueError('No dataset loaded')
+    if index:
+      dataset = Subset(self.dataset, index)
+    else:
+      dataset = self.dataset
+    logger.debug(f"Predicting {len(dataset)} residues")
+    dl = DataLoader(dataset, batch_size=len(dataset), collate_fn=collate_fn)
+    batch = next(iter(dl))
+    with torch.no_grad():
+      output = self.model(batch, output_iscorrect=True,
+                          output_loss=True, output_predicted_rtype=True)
+    return output
+  
+  def get_loss(self):
+    return self.loss.mean().item()
+  
+  def get_accuracy(self):
+    return self.is_correct.float().mean().item()
+  
+  def long_to_short_seq(self, long_seq):
+    """Transform 3 letter sequence to 1 letter sequence."""
+    return ''.join(rc.restype_3to1[residue] for residue in long_seq)
+    
+  def load_file(self, file_path):
+    self.dataset = LocalEnvironmentDataSet(file_path)
+    self.protein = self.dataset.protein
+    self.seq = self.protein.residue_names
+    self.original_seq = self.seq.copy()
+    self._initialize_seq()
+    logger.debug(f"Initialized sequence: {self.seq}")
+    inital_output = self._predict()
+    self.is_correct = inital_output['iscorrect']
+    self.loss = inital_output['loss']
+    self.predicted_rtype = inital_output['predicted_rtype']
+    init_acc = sum((self.seq[i] == self.original_seq[i]
+                    for i in range(len(self.seq)))) / len(self.seq)
+    logger.debug(f"Initial accuracy: {init_acc}")
+    
+  def design(self, seed=42):
+    """Design the sequence."""
+    # Set seed.
+    random.seed(seed)
+    ticker = time.time()
+    niter = 0
+    stage_1_length = int(len(self.seq) / 1)
+    stage_2_length = int(len(self.seq) / 5)
+    schedule = stage_1_length * [5] + stage_2_length * [1]
+    for niter, num_change in enumerate(schedule):
+      logger.debug(f"iter: {niter + 1}")
+      accuracy = self.is_correct.sum().item() / len(self.seq)
+      logger.debug(f"accuracy: {accuracy}")
+      incorrect_index = (~self.is_correct).nonzero().squeeze(-1).tolist()
+      index_to_change = random.sample(incorrect_index, num_change)
+      self._iter(index_to_change)
+    logger.info(f"Designed a sequence of length {len(self.seq)} "
+                f"in {time.time() - ticker:.2f} seconds")
+    identity = sum((self.seq[i] == self.original_seq[i]
+                    for i in range(len(self.seq)))) / len(self.seq)
+    output = {'sequence': self.seq,
+              'loss': self.get_loss(),
+              'accuracy': self.get_accuracy(),
+              'identity': identity}
+    return output
+
+
+def main(args):
+  # print(vars(args))
+  # return
+  # Create model.
+  model = ResidueTypePredictor(
+    args.input_dim, args.d_model, args.nhead, args.nlayer, args.device)
+  # Load model parameters.
+  state_dict = torch.load(args.model_path, map_location=args.device)['state_dict']
+  model.load_state_dict(state_dict)
+  # Start design sequence.
+  designer = SequenceDesigner(model)
+  designer.load_file(args.target_path)
+  output = designer.design()
+  print(output)
+
+  
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  ## Dataset parameters
+  parser.add_argument('-T', '--target_path', type=str,
+                      help='File or directory to evaluate')
+  ## Model parameters
+  parser.add_argument('-D', '--d_model', type=int, default=256,
+                      help='Model dimension, default: 256')
+  parser.add_argument('-H', '--nhead', type=int, default=16,
+                      help='Number of heads, default: 16')
+  parser.add_argument('-L', '--nlayer', type=int, default=3,
+                      help='Number of layers, default: 3')
+  parser.add_argument('-I', '--input_dim', type=int, default=45,
+                      help='Input dimension, default: 45')
+  parser.add_argument('-M', '--model_path', type=str,
+                      help='Path to the model to evaluate')
+  parser.add_argument('-d', '--device', type=str, default='cpu',
+                      help='Device, default: cpu')
+  ## Other parameters
+  parser.add_argument('-l', '--log_level', type=str, default='INFO')
+  parser.add_argument('--log_file', type=str)
+  parser.add_argument('-C', '--config', type=str,
+                      help='Path to JSON config file')
+  
+  # Parse arguments for the first time.
+  args = parser.parse_args()
+  # Load config file for overwriting arguments.
+  if args.config:
+    with open(args.config, 'r') as f:
+        config = json.load(f)
+        config = {k: v for k, v in config.items() if k in args}
+    parser.set_defaults(**config)
+  # Parse arguments again for overwriting config file.
+  args = parser.parse_args()
+  
+  # Check target_path is specified.
+  if args.target_path is None:
+    raise ValueError('Please specify the path to training and evaluation data.')
+  if args.model_path is None:
+    raise ValueError('Please specify the path to the model to evaluate.')
+  args.target_path = os.path.realpath(args.target_path)
+  if args.config:
+    args.config = os.path.realpath(args.config)
+  
+  # Set gloabl log level=
+  logger.setLevel(args.log_level)
+  # Set stream handler
+  log.add_stream_handler(logger, args.log_level)
+  # Set file handler
+  if args.log_file:
+    log.add_file_handler(logger, args.log_file, args.log_level)
+
+  main(args)
